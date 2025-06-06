b4ab99916aa3f41c2a17ea6ef09f8816c5a83d6b
erikhauszner <erikhauszner@icloud.com>
2025-06-06 01:08:43 -0300

    stable

--- test-server.js
+++ test-server.js
@@ -5,7 +5,7 @@ const http = require('http');
 
 // Hacer una petición GET a la API
 const options = {
-  hostname: '147.93.36.93',
+  hostname: 'http://147.93.36.93',
   port: 3000,
   path: '/api/health',
   method: 'GET'
